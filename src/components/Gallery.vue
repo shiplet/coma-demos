baef7cb9f14ab471663b145126448ba088a9fc87
Ship <mnshiplet@gmail.com>
2020-05-19 21:12:41 -0600

    update mobile style

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -5,17 +5,18 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+	import { Component, Vue } from 'vue-property-decorator'
 
-@Component
-export default class Gallery extends Vue {}
+	@Component
+	export default class Gallery extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-div {
-	margin-top: 25px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-}
+	div {
+		margin-top: 25px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+	}
 </style>
